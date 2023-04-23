7a32eaf7601305dd0b97450930466f80557cbf5b
TONGTONGDESHIJIE <1090060206@qq.com>
2023-04-23 20:36:23 +0800

    feat(custom): 基础核心模块common通用类

---
+++ api-base/src/main/java/chen/common/cache/RedisKeys.java
@@ -0,0 +1,24 @@
+package chen.common.cache;
+
+/**
+ * Redis Key管理
+ *
+ * @author chen
+ */
+public class RedisKeys {
+
+    /**
+     * 验证码Key
+     */
+    public static String getCaptchaKey(String key) {
+        return "sys:captcha:" + key;
+    }
+
+    /**
+     * accessToken Key
+     */
+    public static String getAccessTokenKey(String accessToken) {
+        return "sys:access:" + accessToken;
+    }
+
+}
